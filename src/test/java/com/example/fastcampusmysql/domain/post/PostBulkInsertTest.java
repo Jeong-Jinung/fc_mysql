68f764347b6c2c76beb4924f6df0f0ed87586b50
JeongJinUng <jinung.dev@gmail.com>
2024-01-15 01:28:26 +0900

    :sparkles: 인덱스 추가 후 성능 비교

--- src/test/java/com/example/fastcampusmysql/domain/post/PostBulkInsertTest.java
+++ src/test/java/com/example/fastcampusmysql/domain/post/PostBulkInsertTest.java
@@ -20,16 +20,16 @@ class PostBulkInsertTest {
     @Test
     void bulkInsert() {
         var easyRandom = PostFixtureFactory.get(
-            3L,
-            LocalDate.of(2023,1,1),
-            LocalDate.of(2023,1,31)
+            4L,
+            LocalDate.of(1970,1,1),
+            LocalDate.of(2024,2,1)
         );
 
         var stopWatch = new StopWatch();
         stopWatch.start();
 
         int _1만 = 10000;
-        var posts = IntStream.range(0, _1만 * 100)
+        var posts = IntStream.range(0, _1만 * 200)
             .parallel()
             .mapToObj(i -> easyRandom.nextObject(Post.class))
             .toList();
